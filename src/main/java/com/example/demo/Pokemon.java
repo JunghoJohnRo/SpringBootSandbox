43cf4047a9e5ac649bb4afa8fa82f71499bc5549
JunghoJohnRo <junghojohnro@gmail.com>
2021-08-07 15:22:54 -0500

    first commit

---
+++ src/main/java/com/example/demo/Pokemon.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+enum Gender {
+    MALE,
+    FEMALE
+}
+
+public class Pokemon {
+    private String name;
+    private Gender gender;
+    private String type;
+
+    public Pokemon() {
+
+    }
+}
+
